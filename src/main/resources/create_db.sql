b1ff0561084990790948ad0becf5810618f83936
Maxim Kuzmenchuk <46691880+maxkuzmenchuk@users.noreply.github.com>
2022-03-25 12:03:03 +0300

    sql for custom tokenStore

--- src/main/resources/create_db.sql
+++ src/main/resources/create_db.sql
@@ -23,3 +23,21 @@ create table app_user_roles
 alter table app_user_roles
     owner to postgres;
 
+CREATE TABLE oauth_access_token
+(
+    authentication_id varchar(255) NOT NULL PRIMARY KEY,
+    token_id          varchar(255) NOT NULL,
+    token             bytea        NOT NULL,
+    user_name         varchar(255) NOT NULL,
+    client_id         varchar(255) NOT NULL,
+    authentication    bytea        NOT NULL,
+    refresh_token     varchar(255) NOT NULL
+);
+
+CREATE TABLE oauth_refresh_token
+(
+    token_id       varchar(255) NOT NULL,
+    token          bytea        NOT NULL,
+    authentication bytea        NOT NULL
+);
+
